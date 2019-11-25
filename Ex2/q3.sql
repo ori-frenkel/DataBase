3a5729a274f21b879f2c01ee74113f6b3e79000d
ori-frenkel <49747014+ori-frenkel@users.noreply.github.com>
2019-11-25 17:45:55 +0200

    Update q3.sql

--- Ex2/q3.sql
+++ Ex2/q3.sql
@@ -1,9 +1,11 @@
-SELECT pid,pname
+SELECT (pid,pname)
 FROM Doctor, Patient, Visit
 WHERE Doctor.did=Visit.did and Patient.pid=Visit.pid and
 Doctor.specialty="pediatrician"
+ORDER BY (pid,pname) ASC
 INTERSECT
-SELECT pid,pname
+SELECT (pid,pname)
 FROM Doctor, Patient, Visit
 WHERE Doctor.did=Visit.did and Patient.pid=Visit.pid and
 Doctor.specialty="orthopedist";
+ORDER BY (pid,pname) ASC;
