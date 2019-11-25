73cf196936514f1d77b9b5a2bb9563dc0f57d1a6
ori-frenkel <49747014+ori-frenkel@users.noreply.github.com>
2019-11-25 22:56:14 +0200

    Update q4.sql

--- Ex2/q4.sql
+++ Ex2/q4.sql
@@ -1,7 +1,5 @@
 SELECT pid,did
 FROM Doctor NATURAL JOIN Patient
-ORDER BY (pid,did) ASC
 EXCEPT
 SELECT pid,did
-FROM Visit NATURAL JOIN Patient
-ORDER BY (pid,did) ASC;
+FROM Visit NATURAL JOIN Patient;
