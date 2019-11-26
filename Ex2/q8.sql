adbccefb6e0647610c87d7bf2eca1ae40b242508
ori-frenkel <49747014+ori-frenkel@users.noreply.github.com>
2019-11-26 10:14:31 +0200

    Create q8.sql

---
+++ Ex2/q8.sql
@@ -0,0 +1,10 @@
+-- todo, need to handle case where same person came few time to the doc.
+CREATE VIEW Doc_Avg_Pbmi AS
+	SELECT did, AVG(bmi) as doc_avg_bmi
+    FROM Patient NATURAL JOIN Visit
+    GROUP BY did;
+  
+SELECT did
+FROM Doc_Avg_Pbmi
+WHERE doc_avg_bmi = (SELECT MAX(doc_avg_bmi) FROM Doc_Avg_Pbmi)
+ORDER BY did ASC;
